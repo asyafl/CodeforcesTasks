9ceab34946228a0aff5160596c6c887081e21e98
asyafl <65190854+asyafl@users.noreply.github.com>
2021-03-02 20:43:30 +0200

    implement tast using greedy algorithm

---
+++ 996A_HitTheLottery/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace _996A_HitTheLottery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            var l = new Lottery();
+            Console.WriteLine(l.Solve(n));
+        }
+    }
+
+    public class Lottery
+    {
+        private int[] bills = new int[5] { 100, 20, 10, 5, 1 }; 
+        public int Solve(int n)
+        {
+            int result = 0;
+
+            while (n != 0)
+            {
+                foreach(var b in bills)
+                {
+                    int tmp = 0;
+
+                    while ((n - b) >= 0)
+                    {
+                        n -= b;
+                        tmp++;
+                    }
+
+                    result += tmp;
+                }
+            }
+
+            return result;
+        }
+    }
+}
